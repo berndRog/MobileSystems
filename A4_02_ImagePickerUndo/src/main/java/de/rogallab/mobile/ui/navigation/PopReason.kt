4d6b92bcca7ea08beb649d68b8acdd97eec9a6aa
Bernd-Uwe Rogalla <b-u.rogalla@ostfalia.de>
2026-09-08 14:19:27 +0200

    Remove obsolete course modules

--- A4_02_ImagePickerUndo/src/main/java/de/rogallab/mobile/ui/navigation/PopReason.kt
+++
@@ -1,6 +0,0 @@
-package de.rogallab.mobile.ui.navigation
-
-enum class PopReason {
-   Save,
-   Cancel,
-}
--- A4_02_ImagePickerUndo/src/main/java/de/rogallab/mobile/ui/people/create_detail/PersonUiState.kt
+++
@@ -1,9 +0,0 @@
-package de.rogallab.mobile.ui.people.create_detail
-
-import de.rogallab.mobile.domain.entities.Person
-
-data class PersonUiState(
-   val person: Person = Person(),
-   val isNew: Boolean = true,
-   val isLoading: Boolean = false,
-)
