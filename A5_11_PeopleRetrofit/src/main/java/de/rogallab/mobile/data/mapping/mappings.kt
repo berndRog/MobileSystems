3ffa46d45e380ca241e3687e1979d890cd94797a
Bernd-Uwe Rogalla <bu.rogalla@icloud.com>
2026-09-08 17:44:21 +0200

    Use server image URLs directly with adb reverse

--- A5_11_PeopleRetrofit/src/main/java/de/rogallab/mobile/data/mapping/mappings.kt
+++ A5_11_PeopleRetrofit/src/main/java/de/rogallab/mobile/data/mapping/mappings.kt
@@ -1,7 +1,6 @@
 package de.rogallab.mobile.data.mapping
 
 import de.rogallab.mobile.data.remote.dtos.PersonDto
-import de.rogallab.mobile.data.remote.network.ServerUrl
 import de.rogallab.mobile.domain.entities.Person
 
 fun PersonDto.toPerson(): Person =
@@ -10,7 +9,7 @@ fun PersonDto.toPerson(): Person =
       lastName = lastName,
       email = email,
       phone = phone,
-      imagePath = ServerUrl.resolve(imageUrl),
+      imagePath = imageUrl,
       id = id,
    )
 
@@ -19,6 +18,6 @@ fun PersonDto.toPerson(): Person =
  *
  * - Die Mapping-Funktion verhindert, dass Retrofit-/JSON-DTOs die Data-Schicht
  *   verlassen. ViewModels und UI arbeiten weiterhin nur mit Person.
- * - Die vom Server gelieferte ImageUrl wird beim Mapping für den Android Emulator
- *   aufgelöst, falls ihr Host localhost ist.
+ * - Mit adb reverse verwendet die App 127.0.0.1 auch für vom Server erzeugte
+ *   ImageUrls. Eine zusätzliche Host-Umschreibung auf 10.0.2.2 ist nicht nötig.
  */
