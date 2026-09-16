8ce93ce6b0458e939086f48b8503df960a355d8f
Bernd-Uwe Rogalla <bu.rogalla@icloud.com>
2026-09-16 19:36:19 +0200

    Use kotlinx serialization for A5_11 PersonDto

--- A5_11_PeopleImagesRetrofit/src/main/java/de/rogallab/mobile/data/remote/dtos/PersonDto.kt
+++ A5_11_PeopleImagesRetrofit/src/main/java/de/rogallab/mobile/data/remote/dtos/PersonDto.kt
@@ -1,19 +1,23 @@
 package de.rogallab.mobile.data.remote.dtos
 
+import kotlinx.serialization.Serializable
+
 // JSON representation returned by PeopleImagesApi.
+@Serializable
 data class PersonDto(
    val id: String,
    val firstName: String,
    val lastName: String,
-   val email: String?,
-   val phone: String?,
-   val imageUrl: String?,
+   val email: String? = null,
+   val phone: String? = null,
+   val imageUrl: String? = null,
 )
 
 /*
  * Didaktik und Lernziele
  *
  * - PersonDto bildet den JSON-Vertrag der WebAPI ab und bleibt in der Data-Schicht.
+ * - @Serializable erzeugt die für kotlinx.serialization benötigte Serialisierung.
  * - Die Domain verwendet weiterhin Person und kennt weder Retrofit noch JSON.
  * - imageUrl ist ein optionaler String in der Antwort. PeopleImagesApi verwaltet
  *   diesen Wert; Create und Update ändern ihn nicht direkt.
